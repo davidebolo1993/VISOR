27ad96a7da939a0c90e52080ca81b071c4605877
Davide Bolognini <davidebolognini7@gmail.com>
2019-03-20 12:20:49 +0100

    Update HACk.py

--- VISOR/HACk/HACk.py
+++ VISOR/HACk/HACk.py
@@ -1061,47 +1061,6 @@ def run(parser, args):
 	logging.info('Done')
 
 
-
-class CustomFormat(HelpFormatter):
-
-	def _format_action_invocation(self, action):
-
-		if not action.option_strings:
-
-			default = self._get_default_metavar_for_positional(action)
-			metavar, = self._metavar_formatter(action, default)(1)
-			
-			return metavar
-
-		else:
-
-			parts = []
-
-			if action.nargs == 0:
-
-				parts.extend(action.option_strings)
-
-			else:
-
-				default = self._get_default_metavar_for_optional(action)
-				args_string = self._format_args(action, default)
-				
-				for option_string in action.option_strings:
-
-					parts.append(option_string)
-
-				return '%s %s' % (', '.join(parts), args_string)
-
-			return ', '.join(parts)
-
-	def _get_default_metavar_for_optional(self, action):
-
-		return action.dest.upper()
-
-
-
-
-
 def write_unmodified_chromosome(chromosome, seq, output_fasta):
 
 	with open (os.path.abspath(output_fasta), 'a') as faout:
