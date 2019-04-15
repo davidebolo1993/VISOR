40ec726cc10621f636663b9228aba8b0708dad75
Davide Bolognini <davidebolognini7@gmail.com>
2019-04-15 09:39:52 +0200

    Update ssmerger.py

--- scripts/ssmerger.py
+++ scripts/ssmerger.py
@@ -75,6 +75,49 @@ def main():
 
 	os.remove(os.path.abspath(args.output + '/bamstomerge.txt'))
 
+
+
+
+
+class CustomFormat(HelpFormatter):
+
+	def _format_action_invocation(self, action):
+
+		if not action.option_strings:
+
+			default = self._get_default_metavar_for_positional(action)
+			metavar, = self._metavar_formatter(action, default)(1)
+			
+			return metavar
+
+		else:
+
+			parts = []
+
+			if action.nargs == 0:
+
+				parts.extend(action.option_strings)
+
+			else:
+
+				default = self._get_default_metavar_for_optional(action)
+				args_string = self._format_args(action, default)
+				
+				for option_string in action.option_strings:
+
+					parts.append(option_string)
+
+				return '%s %s' % (', '.join(parts), args_string)
+
+			return ', '.join(parts)
+
+	def _get_default_metavar_for_optional(self, action):
+
+		return action.dest.upper()
+
+
+
+
 if __name__ == '__main__':
 
 	main()
