bf47c5a1781893125ae6eb2d46948de8a641a754
Davide Bolognini <davidebolognini7@gmail.com>
2019-05-08 16:45:35 +0200

    Update TestSS.sh

--- Tests/TestSS.sh
+++ Tests/TestSS.sh
@@ -1,7 +1,8 @@
-echo "Creating test files"
+echo "Creating test folder"
+
 mkdir SStest && cd SStest
 
-echo "Download reference"
+echo "Downloading reference"
 wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/GRCh38_full_analysis_set_plus_decoy_hla.fa
 samtools faidx GRCh38_full_analysis_set_plus_decoy_hla.fa
 
@@ -10,13 +11,11 @@ samtools faidx GRCh38_full_analysis_set_plus_decoy_hla.fa chr11 > chr11.fa
 cat chr22.fa chr11.fa > chr11_22.fa
 
 
-echo "Download scripts from VISOR git"
+echo "Downloading scripts from VISOR git"
 
 
 wget https://raw.githubusercontent.com/davidebolo1993/VISOR/master/scripts/ssmerger.py
 wget https://raw.githubusercontent.com/davidebolo1993/VISOR/master/scripts/sscounter.py
-
-
 
 echo "Getting files for simulations from VISOR git"
 
@@ -89,9 +88,6 @@ cd ..
 cd ..
 
 
-echo "Done"
-
-
 echo "Simulating strand-seq data with 5% noise for TANDEM DUPLICATION"
 
 mkdir hetdup
@@ -102,8 +98,6 @@ python ssmerger.py -f hetdup/Results/0 hetdup/Results/1 -s W C -o hetdup/Final
 python sscounter.py -g GRCh38_full_analysis_set_plus_decoy_hla.fa -bam hetdup/Final/WC.srt.bam -o hetdup/Final -l H1_W.H2_C.H2_DUP -c chr22
 
 
-echo "Done"
-
 echo "Simulating strand-seq data with 5% noise for DELETION"
 
 mkdir hetdel
@@ -112,8 +106,6 @@ VISOR HACk -g chr22.fa -bed files/hetdel/VISOR.h1.SVs.bed files/hetdel/VISOR.h2.
 VISOR SHORtS -g chr22.fa -s hetdel/Templates -bed files/hetdel/VISOR.sim.bed -c 1 -t single-strand -n 5.0 -o hetdel/Results -th 7
 python ssmerger.py -f hetdel/Results/0 hetdel/Results/1 -s W C -o hetdel/Final
 python sscounter.py -g GRCh38_full_analysis_set_plus_decoy_hla.fa -bam hetdel/Final/WC.srt.bam -o hetdel/Final -l H1_W.H2_C.H2_DEL -c chr22
-
-echo "Done"
 
 
 echo "Simulating strand-seq data with 5% noise for INVERSION"
@@ -125,9 +117,6 @@ VISOR SHORtS -g chr22.fa -s hetinv/Templates -bed files/hetinv/VISOR.sim.bed -c 
 python ssmerger.py -f hetinv/Results/0 hetinv/Results/1 -s W C -o hetinv/Final
 python sscounter.py -g GRCh38_full_analysis_set_plus_decoy_hla.fa -bam hetinv/Final/WC.srt.bam -o hetinv/Final -l H1_W.H2_C.H2_INV -c chr22
 
-
-echo "Done"
-
 echo "Simulating strand-seq data with 5% noise for INVERTED DUPLICATION"
 
 mkdir hetinvdup
@@ -137,9 +126,6 @@ VISOR SHORtS -g chr22.fa -s hetinvdup/Templates -bed files/hetinvdup/VISOR.sim.b
 python ssmerger.py -f hetinvdup/Results/0 hetinvdup/Results/1 -s W C -o hetinvdup/Final
 python sscounter.py -g GRCh38_full_analysis_set_plus_decoy_hla.fa -bam hetinvdup/Final/WC.srt.bam -o hetinvdup/Final -l H1_W.H2_C.H2_INVDUP -c chr22
 
-
-echo "Done"
-
 echo "Simulating strand-seq data with 5% noise for BALANCED TRANSLOCATION"
 
 mkdir transloc
@@ -148,9 +134,6 @@ VISOR HACk -g chr11_22.fa -bed files/transloc/VISOR.h1.SVs.bed files/transloc/VI
 VISOR SHORtS -g chr11_22.fa -s transloc/Templates -bed files/transloc/VISOR.sim.bed -c 1 -t single-strand -n 5.0 -o transloc/Results -th 7
 python ssmerger.py -f transloc/Results/0 transloc/Results/1 -s W C -o transloc/Final
 python sscounter.py -g GRCh38_full_analysis_set_plus_decoy_hla.fa -bam transloc/Final/WC.srt.bam -o transloc/Final -l H1_W.H2_C.22_11_TRANSLOC -c chr22 chr11
-
-
-echo "Done"
 
 
 echo "Simulating strand-seq data with 5% and SCE in haplotype 1"
