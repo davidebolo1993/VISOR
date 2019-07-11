1b68fc1d484c49aba4cf39a3f2dff6f34030d29f
Davide Bolognini <davidebolognini7@gmail.com>
2019-07-11 22:17:02 +0200

    Update runtest.sh

--- Tests/TestTime/VISOR/runtest.sh
+++ Tests/TestTime/VISOR/runtest.sh
@@ -1,2 +1,2 @@
 VISOR HACk -g example.fna.new -bed VISOR.translated.h1.bed VISOR.translated.h2.bed -o Templates
-VISOR SHORtS -g example.fna.new -bed VISOR.sim.bed -s Templates -o Results -th 1 -c 5 -l 100 -is 300 --noaddtag
+VISOR SHORtS -g example.fna.new -bed VISOR.sim.bed -s Templates -o Results --threads 1 -c 5 -l 100 -is 300 --noaddtag
