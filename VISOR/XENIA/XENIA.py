406af20823e51c316bec182da3802d6db143325f
Davide Bolognini <davidebolognini7@gmail.com>
2019-07-16 13:10:08 +0200

    Update XENIA.py

--- VISOR/XENIA/XENIA.py
+++ VISOR/XENIA/XENIA.py
@@ -21,10 +21,12 @@ import numpy as np
 
 global barcodes
 global organizerpath
+global DictTag
 
 def run(parser,args):
 
 	global barcodes
+	global DictTag
 
 	if not os.path.exists(os.path.abspath(args.output)):
 
@@ -80,7 +82,27 @@ def run(parser,args):
 		sys.exit(1)
 
 
-	barcodepath=os.path.abspath(os.path.dirname(__file__) + '/4M-with-alts-february-2016.txt.gz')
+	if args.type=='bulk':
+
+		barcodepath=os.path.abspath(os.path.dirname(__file__) + '/4M-with-alts-february-2016.txt.gz')
+	
+
+	else:
+
+		barcodepath=os.path.abspath(os.path.dirname(__file__) + '/737K-crdna-v1.txt.gz')
+		tagpath=os.path.abspath(os.path.dirname(__file__) + '/chromium-shared-sample-indexes-plate.csv')
+		
+		DictTag=dict()
+
+		with open(tagpath, 'r') as filein:
+
+			counter=1
+
+			for line in filein:
+
+				DictTag[str(counter)]=line.rsplit('\n')[0].split(',')[1:]
+				counter+=1
+
 
 	with gzip.open(barcodepath, 'rt') as fin:
 
@@ -183,7 +205,11 @@ def run(parser,args):
 
 		i=0
 
+		CellsIDX=random.choices(range(0,96),k=args.cells_number)
+		
 		while i < args.cells_number:
+
+			IDX=CellsIDX[i]
 
 			print('Simulating from cell ' + str(i+1))
 
@@ -265,7 +291,9 @@ def run(parser,args):
 
 					print(Par)
 
-					SingleCellSim(Par, str(entries[0]), SEQREGION, REGIONSTART, os.path.abspath(args.output + '/cell' + str(i+1)+'/h' + str(folder+1)), str(i+1), assigned_barcode)
+					print('Chosen well ' + str(CellsIDX[i]) + ' for cell ' + str(i+1))
+
+					SingleCellSim(Par, str(entries[0]), SEQREGION, REGIONSTART, os.path.abspath(args.output + '/cell' + str(i+1)+'/h' + str(folder+1)), str(i+1), IDX, assigned_barcode)
 
 			i+=1
 
@@ -321,6 +349,35 @@ def run(parser,args):
 
 		print('Storing in proper format for Cell Ranger')
 
+		subdirs=sorted([os.path.join(os.path.abspath(args.output), o) for o in os.listdir(os.path.abspath(args.output)) if os.path.isdir(os.path.join(os.path.abspath(args.output),o))], key=natural_keys)
+
+		counter=1
+
+		for i in range(len(fastas)):
+
+			R1=[]
+			R2=[]
+
+			for dirs in subdirs:
+
+				R1.extend(glob.glob(os.path.abspath(dirs) + '/h' + str(i+1) + '/*R1*'))
+				R2.extend(glob.glob(os.path.abspath(dirs) + '/h' + str(i+1) + '/*R2*'))
+
+			command1 = 'cat ' + ' '.join(x for x in sorted(R1))
+			command2 = 'cat ' + ' '.join(x for x in sorted(R2))
+
+			with open(os.path.abspath(args.output + '/' + Par.NAME + '_S1_L' + str(counter).zfill(3) + '_R1_001.fastq'), 'w') as fout:
+
+				subprocess.call(shlex.split(command1), stdout=fout)
+
+			with open(os.path.abspath(args.output + '/' + Par.NAME + '_S1_L' + str(counter).zfill(3) + '_R2_001.fastq'), 'w') as fout:
+
+				subprocess.call(shlex.split(command2), stdout=fout)
+
+			counter +=1
+
+
+		subprocess.call(['bash', cleanerpath, os.path.abspath(args.output)]) #refine FASTQ so that they are readily usable with longranger
 
 
 
@@ -607,35 +664,74 @@ def LinkedSim(Par,reftitle,refseq,refstart, output):
 
 
 
-def SingleCellSim(Par,reftitle,refseq,refstart, output, cell, barcode):
+def SingleCellSim(Par,reftitle,refseq,refstart, output, cell, IDX,barcode):
 
+
+	global DictTag
 
 	truedim=len(refseq)-refseq.count('N')
 	NUM_READS=int(truedim*Par.COV)/(Par.SRL*2)
 
 	print('Simulating ' + str(NUM_READS) + ' reads')
 
-	subprocess.call(['wgsim', '-e', str(Par.SRE), '-d', str(Par.INS), '-s', str(Par.STDEV), '-N', str(NUM_READS), '-1', str(Par.SRL-(16+6)), '-2', str(Par.SRL), '-R', str(Par.INDELP), '-X', str(Par.INDELEXT), os.path.abspath(output + '/'+ str(Par.COUNT) + '.region.tmp.fa'), os.path.abspath(output + '/'+ str(Par.COUNT) + '.R1.fq.tmp'),os.path.abspath(output + '/'+ str(Par.COUNT) + '.R2.fq')], stderr=open(os.devnull, 'wb'), stdout=open(os.devnull, 'wb'))
+	subprocess.call(['wgsim', '-e', str(Par.SRE), '-d', str(Par.INS), '-s', str(Par.STDEV), '-N', str(NUM_READS), '-1', str(Par.SRL-(16)), '-2', str(Par.SRL), '-R', str(Par.INDELP), '-X', str(Par.INDELEXT), os.path.abspath(output + '/'+ str(Par.COUNT) + '.region.tmp.fa'), os.path.abspath(output + '/'+ str(Par.COUNT) + '.R1.fq.tmp'),os.path.abspath(output + '/'+ str(Par.COUNT) + '.R2.fq.tmp')], stderr=open(os.devnull, 'wb'), stdout=open(os.devnull, 'wb'))
 
-	RANDOM6MER=''.join(np.random.choice(['A','T','G','C','N'],6, replace=True))
+	#RANDOM6MER=''.join(np.random.choice(['A','T','G','C','N'],6, replace=True))
+
+	randomized=[]
+
 
 	with open(os.path.abspath(output + '/'+ str(Par.COUNT) + '.R1.fq.tmp'), 'r') as fin, open(os.path.abspath(output + '/'+ str(Par.COUNT) + '.R1.fq'), 'w') as fout:
 
-		for line in fin:
+		for counter,line in enumerate(fin):
+
+			randomized.append(random.choice(DictTag[str(IDX)]))
 
 			if line.startswith(str(5)):
 
-				fout.write(str(5)*(16+6) + line)
+				fout.write(str(5)*(16) + line)
 
-			elif line.startswith('@') or line.startswith('+'):
+			elif line.startswith('@'):
+
+				fout.write('@A00228:100:HCKNNDMXX:' + str(Par.HAPLONUM) + ':1101:19623:2190 1:N:0:' + randomized[counter] + '\n')
+
+			elif line.startswith('+'):
 
 				fout.write(line)
 
 			else:
 
-				fout.write(barcode + RANDOM6MER + line)
+				fout.write(barcode + line)
+
+
+	with open(os.path.abspath(output + '/'+ str(Par.COUNT) + '.R2.fq.tmp'), 'r') as fin, open(os.path.abspath(output + '/'+ str(Par.COUNT) + '.R2.fq'), 'w') as fout:
+
+		for counter,line in enumerate(fin):
+
+			if line.startswith(str(5)):
+
+				fout.write(str(5)*(16) + line)
+
+			elif line.startswith('@'):
+
+				fout.write('@A00228:100:HCKNNDMXX:' + str(Par.HAPLONUM) + ':1101:19623:2190 2:N:0:' + randomized[counter] + '\n')
+
+			elif line.startswith('+'):
+
+				fout.write(line)
+
+			else:
+
+				fout.write(barcode + line)
+
+
 
 	os.remove(os.path.abspath(output + '/'+ str(Par.COUNT) + '.R1.fq.tmp'))
+	os.remove(os.path.abspath(output + '/'+ str(Par.COUNT) + '.R2.fq.tmp'))
+	os.remove(os.path.abspath(output + '/'+ str(Par.COUNT) + '.region.tmp.fa'))
+	os.remove(os.path.abspath(output + '/'+ str(Par.COUNT) + '.region.tmp.fa.fai'))
+
 
 	print('Done')
+
 
