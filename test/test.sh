e354ad146a44711b04c676803f8f20baf593c2dc
davidebolo1993 <davidebolognini7@gmail.com>
2020-12-18 15:17:33 +0100

    fixing travis test

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-
+echo "Checking executable installation"
+echo ""
+echo ""
+samtools --version
+minimap2 --version
+bedtools --version
+echo ""
+echo ""
 echo "Checking VISOR installation"
 echo ""
 echo ""
@@ -15,29 +22,29 @@ done
 samtools faidx GRCh38_full_analysis_set_plus_decoy_hla.fa chr1 > smallref.fa
 samtools faidx GRCh38_full_analysis_set_plus_decoy_hla.fa chr2 >> smallref.fa
 VISOR HACk -h
-VISOR HACk -g smallref.fa -b hack.bed -o hack_out
+VISOR HACk -g smallref.fa -b test/hack.bed -o test/hack_out
 echo ""
 echo ""
 echo "Testing VISOR SHORtS"
 echo ""
 echo ""
 VISOR SHORtS -h
-VISOR SHORtS -g smallref.fa -b shorts.bed -s hack_out -o shorts_out_singleclone --coverage 1 #this is just for testing, no need to spend a lot of time on this
-VISOR SHORtS -g smallref.fa -b shorts.bed -s hack_out hack_out -o shorts_out_multiclone --coverage 1 --clonefraction 50 50 --tag #this is just for testing, no need to spend a lot of time on this
-VISOR SHORtS -g smallref.fa -b shorts.bed -s hack_out -o shorts_out_strandseq --coverage 1 --strandseq --cells 1 --sce sce.bed --noise 5.0
+VISOR SHORtS -g smallref.fa -b test/shorts.bed -s test/hack_out -o test/shorts_out_singleclone --coverage 1 #this is just for testing, no need to spend a lot of time on this
+VISOR SHORtS -g smallref.fa -b test/shorts.bed -s test/hack_out test/hack_out -o test/shorts_out_multiclone --coverage 1 --clonefraction 50 50 --tag #this is just for testing, no need to spend a lot of time on this
+VISOR SHORtS -g smallref.fa -b test/shorts.bed -s test/hack_out -o test/shorts_out_strandseq --coverage 1 --strandseq --cells 1 --sce test/sce.bed --noise 5.0
 echo ""
 echo ""
 echo "Testing VISOR LASeR"
 echo ""
 echo ""
 VISOR LASeR -h
-VISOR LASeR -g smallref.fa -b shorts.bed -s hack_out -o laser_out_singleclone --coverage 0.1 #this is just for testing, no need to spend a lot of time on this
-VISOR LASeR -g smallref.fa -b shorts.bed -s hack_out hack_out -o laser_out_multiclone --coverage 0.1 --clonefraction 50 50 --tag #this is just for testing, no need to spend a lot of time on this
+VISOR LASeR -g smallref.fa -b test/shorts.bed -s test/hack_out -o test/laser_out_singleclone --coverage 0.01 #this is just for testing, no need to spend a lot of time on this
+VISOR LASeR -g smallref.fa -b test/shorts.bed -s test/hack_out test/hack_out -o test/laser_out_multiclone --coverage 0.01 --clonefraction 50 50 --tag #this is just for testing, no need to spend a lot of time on this
 echo ""
 echo ""
 echo "Testing VISOR XENIA"
 echo ""
 echo ""
-VISOR XENIA -s hack_out/ -b shorts.bed -o xenia_out --coverage 1 #this is just for testing, no need to spend a lot of time on this
+VISOR XENIA -s test/hack_out -b test/shorts.bed -o test/xenia_out --coverage 1 #this is just for testing, no need to spend a lot of time on this
 echo ""
 echo ""
