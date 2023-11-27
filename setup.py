636aef411fce584341e0a02a99c5995cf8365f97
davidebolo1993 <davidebolognini7@gmail.com>
2023-11-27 14:23:48 +0100

    secr

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup,find_packages
 
-setup(name='VISOR',      
+setup(name='VISOR',
   version=1.1,
   description='VarIant SimulatOR',
   url='https://github.com/davidebolo1993/VISOR.git',
